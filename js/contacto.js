51ff816811519c24686765368384eb8fa615036c
Yilin <yilin.zh135@gmail.com>
2024-10-27 23:33:22 +0100

    Add location script

--- js/contacto.js
+++ js/contacto.js
@@ -1,12 +1,39 @@
-const masterDCoords = [40.441232, -3.7000116]; 
 
-        const map = L.map('map').setView(masterDCoords, 12);
 
-        L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-            maxZoom: 19,
-            attribution: '© OpenStreetMap contributors'
-        }).addTo(map);
+const map = L.map('map').setView([40.441232, -3.7000116], 12); // Coordenadas de MasterD
 
-        L.marker(masterDCoords).addTo(map)
-            .bindPopup('Ubicación de MasterD')
-            .openPopup();
+L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '© OpenStreetMap contributors'
+}).addTo(map);
+
+// Función para obtener la ubicación del usuario
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition, showError);
+    } else {
+        showError();
+    }
+}
+
+// Mostrar la posición del usuario en el mapa
+function showPosition(position) {
+    const userLat = position.coords.latitude;
+    const userLon = position.coords.longitude;
+    
+    map.setView([userLat, userLon], 12);
+    
+    L.marker([userLat, userLon]).addTo(map)
+        .bindPopup('Tu ubicación')
+        .openPopup();
+}
+
+function showError() {
+    map.setView([40.441232, -3.7000116], 12);
+    
+    L.marker([40.441232, -3.7000116]).addTo(map)
+        .bindPopup('Ubicación de MasterD')
+        .openPopup();
+}
+
+getLocation();
